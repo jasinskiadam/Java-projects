afe242b472c6b44dbd91e7541f8cc4ae6523e7b4
jasinskiadam <38188852+jasinskiadam@users.noreply.github.com>
2022-11-07 00:39:15 +0100

    feat: add Class

--- src/Class.java
+++ src/Class.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Class {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+import java.util.Collections;
+public class Class implements Comparator<String> {
+    String group_name;
+    List<Student> students = new ArrayList<>();
+    int max_students;
+
+    Class(String group_name, int max_students) {
+        this.group_name = group_name;
+        this.max_students = max_students;
+    }
+
+    @Override
+    public int compare(String s1, String s2){
+        return s1.compareToIgnoreCase(s2);
+    }
 }
+
