1b1f7f7028f34b63e2b4458ed3ceace480eb88bc
jasinskiadam <38188852+jasinskiadam@users.noreply.github.com>
2022-11-07 01:00:33 +0100

    feat: add ClassContainer

---
+++ src/ClassContainer.java
@@ -0,0 +1,10 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class ClassContainer {
+    Map<String, Class> groups;
+
+    ClassContainer(){
+        this.groups = new HashMap<String, Class>();
+    }
+}
