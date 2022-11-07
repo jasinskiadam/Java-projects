8a0bbb54b8feac8b137bb86cd9f38915cb533fc4
jasinskiadam <38188852+jasinskiadam@users.noreply.github.com>
2022-11-07 01:02:40 +0100

    feat: add findEmpty method

--- src/ClassContainer.java
+++ src/ClassContainer.java
@@ -1,5 +1,7 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ClassContainer {
     Map<String, Class> groups;
@@ -14,5 +16,15 @@ public class ClassContainer {
 
     public void removeClass(String group_name){
         this.groups.remove(group_name);
+    }
+
+    public List<String> findEmpty(){
+        List<String> result = new ArrayList<>();
+        for(String key: this.groups.keySet()){
+            if(this.groups.get(key).students.isEmpty()){
+                result.add(key);
+            }
+        }
+        return result;
     }
 }
