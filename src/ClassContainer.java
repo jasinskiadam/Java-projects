44637590f20962811582b6261cb7c927d24ad337
jasinskiadam <38188852+jasinskiadam@users.noreply.github.com>
2022-11-07 01:01:15 +0100

    feat: add addClass method

--- src/ClassContainer.java
+++ src/ClassContainer.java
@@ -7,4 +7,8 @@ public class ClassContainer {
     ClassContainer(){
         this.groups = new HashMap<String, Class>();
     }
+
+    public void addClass(String group_name, int max_students){
+        this.groups.put(group_name, new Class(group_name, max_students));
+    }
 }
